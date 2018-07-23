de29b503098c35cc824408f97a038d514b84605c
Oleg01rus01 <41576522+Oleg01rus01@users.noreply.github.com>
2018-07-23 17:29:41 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-var TelegramBot = require('node-telegram-bot-api');
-const token = '644425603:AAFsR-MIvFsv1hKGM8CVZe1YQSDpTW83LUY';
-var bot = new TelegramBot(token, {polling: true});
-var notes = [];
-bot.on('message', msg=>{
-	bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}, your id ${msg.from.id}`);
-})
-
-/* bot.onText(/напомни (.+) в (.+)/,function(msg,match){
-	var userId=msg.from.id;
-	var text=match[1];
-	var time=match[2];
-
-	notes.push({'uid':userId,'time':time,'text':text});
-	bot.sendMessage(userId,'Отлично! Я обязательно напомню, если не сдохну :)');
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = '644425603:AAFsR-MIvFsv1hKGM8CVZe1YQSDpTW83LUY';
+const bot = new TelegramBot(TOKEN, {polling: true});
+//var notes = [];
+bot.on('message', msg => {
+	bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name} your id ${msg.from.id}`);
 });
 
-setInterval(function(){
-	for(var i=0;i<notes.length;i++){
-		var curDate=new Date().getHours()+':'+new Date().getMinutes();
-		if(notes[i]['time']==curDate){
-			bot.sendMessage(notes[i]['uid'],'Напоминаю, что вы должны: '+notes[i]['text']+' сейчас.');
-			notes.splice(i,1);}
-		}
-	},1000
-); */
+
 
